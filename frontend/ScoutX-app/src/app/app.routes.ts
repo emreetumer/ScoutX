787faa1ca757b56bf6b09046a757b19d446dd2bc
Emre <152513816+emreetumer@users.noreply.github.com>
2025-06-07 20:02:44 +0300

    frontend commiti yanlışlık olud.

--- frontend/ScoutX-app/src/app/app.routes.ts
+++ frontend/ScoutX-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PlayersComponent } from './pages/players/players.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: 'players',
+    component: PlayersComponent,
+  },
+  {
+    path:'',
+    redirectTo:'players',
+    pathMatch:'full'
+  },
+];
